eb54e59bfc5c82928cd6c152af2b31f76feff253
O-sulloc <bujjaf@gmail.com>
2022-12-14 17:17:37 +0900

    add post

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "나의 첫번째 글",
+        "excerpt":"머리말 작성 글을 작성하기에 앞서 머리말을 작성해주어야 한다. --- title: \"여기에 제목을 적어\" excerpt: \"여기에는 한 줄 요약\" categories: - Blog //여기에 카테고리명을 적어 tags: - [Blog, jekyll, Github, Git] //이건 말그대로 태그들 toc: true //목차 나오게 할 건지? toc_sticky: false //스크롤 하는 대로 목차 따라다니게 할 건지 date: 2022-12-14...","categories": ["Blog"],
+        "tags": [],
+        "url": "/blog/first-post/",
+        "teaser": null
+      }]

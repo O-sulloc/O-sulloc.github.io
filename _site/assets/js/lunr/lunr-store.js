1e4124b25b838b0b1a1f20ca815fb2b133a87fda
O-sulloc <bujjaf@gmail.com>
2023-01-25 12:01:21 +0900

    aws-ec2 새로운 글 업로드

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "[mac] AWS EC2에 docker 설치",
+        "excerpt":"다음을 참고했습니다. 📖 김담형. 『서비스 운영이 쉬워지는 AWS 인프라 구축 가이드』. 위키북스, 201 Aws Ec2 띄우고 서버에 들어가서 Docker설치하기 EC2? Elastic Compute Cloud 원하는 시간만큼 사용할 수 있는 ‘가상 서버’. CentOS, Ubuntu, Windows 등 대부분 OS 지원. 인스턴스 생성 amazon ec2 접속 인스턴스 시작 주황색 인스턴스 시작 눌러 이름, 우분투,...","categories": ["aws"],
+        "tags": ["aws","ec2","docker"],
+        "url": "/aws/aws-ec2/",
+        "teaser": null
+      },{
         "title": "나의 첫번째 글",
         "excerpt":"머리말 작성 글을 작성하기에 앞서 머리말을 작성해주어야 한다. --- title: \"여기에 제목을 적어\" excerpt: \"여기에는 한 줄 요약\" categories: - Blog //여기에 카테고리명을 적어 tags: - [Blog, jekyll, Github, Git] //이건 말그대로 태그들 toc: true //목차 나오게 할 건지? toc_sticky: false //스크롤 하는 대로 목차 따라다니게 할 건지 date: 2022-12-14...","categories": ["git"],
         "tags": ["git","markdown"],
